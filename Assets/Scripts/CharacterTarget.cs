9f68676f20fcb27d29a6252a4dfee641a187b4dd
Saturn1B <60229050+Saturn1B@users.noreply.github.com>
2025-04-05 12:14:38 +0200

    switch to urp

---
+++ Assets/Scripts/CharacterTarget.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterTarget : MonoBehaviour
+{
+    private Transform playerCamera;
+    private CharacterMovement characterMovement;
+    [SerializeField] private int playerReach;
+
+	private void Start()
+	{
+		playerCamera = GetComponentInChildren<Camera>().transform;
+		characterMovement = GetComponent<CharacterMovement>();
+	}
+
+	private void Update()
+	{
+		RaycastHit hit;
+
+		if (Physics.Raycast(playerCamera.position, playerCamera.forward, out hit, playerReach))
+		{
+			if (hit.transform.GetComponent<InteractionObject>())
+			{
+				if (Input.GetKeyDown(KeyCode.E))
+				{
+					InteractionObject interact = hit.transform.GetComponent<InteractionObject>();
+					interact._interactionEvent.Invoke();
+				}
+			}
+		}
+	}
+}
