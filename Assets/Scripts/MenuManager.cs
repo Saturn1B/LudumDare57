b0618c2a16d952224d3ac28c1e6ef9f9f9dc7be1
Saturn1B <60229050+Saturn1B@users.noreply.github.com>
2025-04-06 18:20:09 +0200

    added main menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject MainPanel, ExplanationPanel, CreditsPanel, OptionsPanel;
+
+
+    [SerializeField] Slider soundSlider, musicSlider;
+    [SerializeField] AudioMixer soundMixer, musicMixer;
+
+    public void StartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("TestingScene", LoadSceneMode.Single);
+    }
+
+    public void Credits()
+    {
+        MainPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+    }
+
+    public void Options()
+    {
+        MainPanel.SetActive(false);
+        OptionsPanel.SetActive(true);
+
+        float soundValue;
+        soundMixer.GetFloat("Sound", out soundValue);
+        soundValue = Mathf.Pow(10, (soundValue / 20));
+        soundSlider.value = soundValue;
+
+        float musicValue;
+        musicMixer.GetFloat("Music", out musicValue);
+        musicValue = Mathf.Pow(10, (musicValue / 20));
+        musicSlider.value = musicValue;
+    }
+
+    public void BackToMain(GameObject panelToClose)
+    {
+        panelToClose.SetActive(false);
+        MainPanel.SetActive(true);
+    }
+
+    public void SetSoundLevel(float sliderValue)
+    {
+        soundMixer.SetFloat("Sound", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetMusicLevel(float sliderValue)
+    {
+        musicMixer.SetFloat("Music", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
